3d6baef0d6c1007c1f349474394a99c0491ac572
Удел Плебеев <valerij.tehnik@yandex.ru>
2023-07-19 15:59:31 +0300

    Database connection

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-
-const express = require('express')
+const dotenv = require('dotenv').config();
+const express = require('express');
+const { createConnection } = require('typeorm');
 
 const app = express()
 
 const PORT = 3000
 
-app.listen(PORT, () => console.log("Server has been started on port " + PORT))
+createConnection({
+  type: process.env.DATABASE_TYPE,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: true,
+}).then(connection => {
+
+  app.listen(PORT, () => console.log("Server has been started on port " + PORT))
+  
+}).catch(error => {
+  console.log("Database connection error:", error)
+});

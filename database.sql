6296230888c7e9cb16b81d79ef4c508ce75f1257
Kamal <75329625+Hubulbib@users.noreply.github.com>
2023-08-02 09:57:01 +0300

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-create TABLE client(
-    id SERIAL PRIMARY KEY,
-    fullName VARCHAR(100),
-    passport VARCHAR(100),
-    phone VARCHAR(100),
-    room VARCHAR(10),
-    arrival DATE,
-    departure DATE
-);
